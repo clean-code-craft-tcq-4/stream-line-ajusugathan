9159d10be0b6f2c9e0e4eba84fc5b31af92bb936
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-09-04 17:44:22 +0530

    Update BMS_DataSender.c

--- Sender/BMS_DataSender.c
+++ Sender/BMS_DataSender.c
@@ -1,20 +1,33 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "BMS_DataSender.h"
 
+/*
+ **********************************************************************************************************************
+ * Service name         : Generate_SensorData
+ * Syntax               : OpeartionStatus_en Generate_SensorData(int DataSize,DataStructure *SensorData)
+ * param[in]            : DataSize    (total data size)
+ * param[in]            : SensorData (pointer to the data holding structure)
+ * return               : DATA_GENERATION_FAILED, DATA_GENERATION_OK
+ * Description          : Function to generate random Temperature and SOC values 
+ 
+ Note :-Please set the maximum possible data size in BMS_DataSender.h,default value is 100
+ **********************************************************************************************************************
+ */
 
 OpeartionStatus_en Generate_SensorData(int DataSize,DataStructure *SensorData)
 {
 	OpeartionStatus_en GenerationStatus=DATA_GENERATION_FAILED;
 
-	printf("\n Generating Temperature and State Of Charge\n");
 	if((DataSize)&&(SensorData!=NULL))
 	{
+		printf("\n**Generating Temperature and State Of Charge**\n\n");
 		GenerationStatus=DATA_GENERATION_OK;
 		for(int count=0;count<DataSize;count++)
-		{
+		{ 
+	        /*REQ_TEMPEARTURE_RANGE value can be adjusted by user in BMS_DataSender.h,default value is 100*/
 			SensorData->Tempearature[count]=rand()%REQ_TEMPEARTURE_RANGE;
+			/*REQ_SOC_RANGE value can be adjusted by user in BMS_DataSender.h,default value is 100*/
 			SensorData->Soc[count]=rand()%REQ_SOC_RANGE;
 		}
 	}
@@ -22,6 +35,19 @@ OpeartionStatus_en Generate_SensorData(int DataSize,DataStructure *SensorData)
 	return(GenerationStatus);
 }
 
+/*
+ **********************************************************************************************************************
+ * Service name         : PrintData
+ * Syntax               : void PrintData(int DataSize,DataStructure *SensorData)
+ * param[in]            : DataSize    (total data size)
+ * param[in]            : SensorData (pointer to the data holding structure)
+ * return               : Nill
+ * Description          : Function to send/print the value in console
+  
+ Note :-Please set the maximum possible data size in BMS_DataSender.h,default value is 100
+ **********************************************************************************************************************
+ */
+ 
 void PrintData(int DataSize,DataStructure *SensorData)
 {
     for(int count=0;count<DataSize;count++)
@@ -30,6 +56,18 @@ void PrintData(int DataSize,DataStructure *SensorData)
 	}
 }
 
+/*
+ **********************************************************************************************************************
+ * Service name         : Send_SensorData
+ * Syntax               : OpeartionStatus_en Send_SensorData(int DataSize,DataStructure *SensorData)
+ * param[in]            : DataSize    (total data size)
+ * param[in]            : SensorData (pointer to the data holding structure)
+ * return               : DATA_SENDING_FAILED, DATA_SENDING_OK
+ * Description          : Function to generate and send Temperature , SOC values to the console 
+  
+ Note :-Please set the maximum possible data size in BMS_DataSender.h,default value is 100
+ **********************************************************************************************************************
+ */
 OpeartionStatus_en Send_SensorData(int DataSize,DataStructure *SensorData)
 {
 	OpeartionStatus_en GenerationStatus=DATA_SENDING_FAILED;
