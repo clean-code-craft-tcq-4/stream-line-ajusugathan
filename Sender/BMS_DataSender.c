d91f00c923abfa1c980fe1c3d967bc5518d85eba
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-09-04 17:10:00 +0530

    Update BMS_DataSender.c

--- Sender/BMS_DataSender.c
+++ Sender/BMS_DataSender.c
@@ -1,1 +1,47 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "BMS_DataSender.h"
+
+
+OpeartionStatus_en Generate_SensorData(int DataSize,DataStructure *SensorData)
+{
+	OpeartionStatus_en GenerationStatus=DATA_GENERATION_FAILED;
+
+	printf("\n Generating Temperature and State Of Charge\n");
+	if((DataSize)&&(SensorData!=NULL))
+	{
+		GenerationStatus=DATA_GENERATION_OK;
+		for(int count=0;count<DataSize;count++)
+		{
+			SensorData->Tempearature[count]=rand()%REQ_TEMPEARTURE_RANGE;
+			SensorData->Soc[count]=rand()%REQ_SOC_RANGE;
+		}
+	}
+
+	return(GenerationStatus);
+}
+
+void PrintData(int DataSize,DataStructure *SensorData)
+{
+    for(int count=0;count<DataSize;count++)
+	{
+		printf("%2f,%2f\n",SensorData->Tempearature[count],SensorData->Soc[count]);
+	}
+}
+
+OpeartionStatus_en Send_SensorData(int DataSize,DataStructure *SensorData)
+{
+	OpeartionStatus_en GenerationStatus=DATA_SENDING_FAILED;
+    if(SensorData!=NULL)
+	{
+		GenerationStatus=Generate_SensorData(DataSize,SensorData);
+		if(GenerationStatus)
+		{
+			PrintData(DataSize,SensorData);
+			GenerationStatus=DATA_SENDING_OK;
+		}
+	}
+	return(GenerationStatus);
+}
+
