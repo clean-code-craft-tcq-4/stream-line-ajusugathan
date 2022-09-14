e1dd6751ce8641e32022166059c060d71798217a
APAC\CJA2KOR <cja2kor@bosch.com>
2022-09-14 09:47:58 +0530

    Add Receiver files and Update workflows

---
+++ Test_Receiver.cpp
@@ -0,0 +1,31 @@
+#define CATCH_CONFIG_MAIN
+
+#include "test/catch.hpp"
+#include <iostream>
+#include "BMS_DataReceiver.hpp"
+#include <cstdlib> 
+#include <ctime> 
+
+
+
+//TEST_CASE("TEST_CASE 1 :: Generate Parameters of Battery Temperature and SOC") {
+//  
+//    Battery batteryobj;
+//    std::srand(static_cast<unsigned int>(std::time(nullptr))); 
+//  
+//    for (int count=1; count <= 100; ++count)
+//    {
+//        assert(batteryobj.batteryIsOk((std::rand()%100),TEMPERATURE) == true);
+//	}
+//	for (int count=1; count <= 100; ++count)
+//    {
+//        assert(batteryobj.batteryIsOk((std::rand()%100),SOC) == true);
+//	}
+//	assert(batteryobj.printoutputtocommunicate() == true);
+//}
+//TEST_CASE("TEST_CASE 2 :: Fail to Generate Parameters of Battery") {
+//  
+//    Battery batteryobj;
+//    std::srand(static_cast<unsigned int>(std::time(nullptr))); 
+//	assert(batteryobj.printoutputtocommunicate() == false);
+//}
