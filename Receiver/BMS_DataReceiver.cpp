ecb986947469bb9b7a31355d6598310592f4d1d1
APAC\CJA2KOR <cja2kor@bosch.com>
2022-09-16 09:03:56 +0530

    Reading Data From Stdin

--- Receiver/BMS_DataReceiver.cpp
+++ Receiver/BMS_DataReceiver.cpp
@@ -2,9 +2,9 @@
 
 bool readingfromstdin()
 {
-   std::string line; 
-   std::getline(std::cin, line);
-   std::cout << line << std::endl;
+	 std::string line; std::getline(std::cin, line);) {
+       std::cout << line << std::endl;
+    }
 	return true;
 	
 }
